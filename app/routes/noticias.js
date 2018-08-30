5fe8a945d4c929de0078da0c645c5500c4d6d36e
Carlos Novack <carlosnovack@gmail.com>
2018-08-30 11:37:22 -0300

    Conexão com MySQL
    Signed-off-by: Carlos Novack <carlosnovack@gmail.com>

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,20 @@
 module.exports = function(app) {
+
 	app.get('/noticias', function(req, res){
-		res.render("noticias/noticias");
+
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'root',
+			password : '123456',
+			database : 'portal_noticias'
+		});
+
+connection.query('select * from noticia', function(err, result){
+    console.log(result);
+    console.log(err);
+    res.send(result);
+});
 	});
 };
