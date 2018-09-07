7d5365953e883a4dba7d631f37abce33f4c414b6
Carlos Novack <carlosnovack@gmail.com>
2018-09-07 15:19:02 -0300

    New 1
    Signed-off-by: Carlos Novack <carlosnovack@gmail.com>

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+MSYS 2 = Terminal
+ls = List = Listar arquivos da pasta
+cd = Change Directory = Mudar pasta
+mv = Mover Arquivos
+rm -rf = Remover Pasta
+
+Front End = Navegadores
+Back End = Servidor
+
+Request = Solicitação / Response = Resposta para a Solicitação / Require = Responsável por retornar o que foi exportado de outro módulo / { } = Objeto
+
---
+++ config/dbConnection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var connMySQL = function(){
+	console.log('A Conexão com o BD foi estabelecida');
+	return mysql.createConnection({
+      host : process.env.MYSQL_SERVER,
+      user : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database :process.env.MYSQL_DATABASE
+		});
+}
+
+module.exports = function() {
+	console.log('O autoload carregou o módulo de conexão com o BD')
+	return connMySQL;
+	
+}
