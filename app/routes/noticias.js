a105efd514c1a26408c94f133dde782cc6fe9710
Luis Valdes <luischivaldes@gmail.com>
2018-09-07 15:39:38 -0300

    Fix merge conflict and add .env to ignore file
    Signed-off-by: Luis Valdes <luischivaldes@gmail.com>

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -5,17 +5,12 @@ module.exports = function(application) {
 		var connection = application.config.dbConnection();
 		var noticiasModel = application.app.models.noticiasModel;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-		noticiasModel.getNoticias(connection, function(error, result){
-=======
     noticiasModel.getNoticias(connection, function(error, result){
->>>>>>> 07faf6833b425c9e7edcab1eb02b59cf1c8727f1
 		    console.log(result);
 		    console.log(error);
 		    res.render("noticias/noticias", {noticias : result});
 		});
-=======
+
     var connection = mysql.createConnection({
       host: process.env.MYSQL_SERVER,
       user: process.env.MYSQL_USER,
@@ -23,16 +18,5 @@ module.exports = function(application) {
       database: process.env.MYSQL_DATABASE
     });
 
-<<<<<<< HEAD
-    connection.query('select * from noticia', function(err, result){
-        console.log(result);
-        console.log(err);
-        res.send(result);
-    });
-
->>>>>>> 78717668d01a153642800a8ea7b2e125084c5d00
-=======
->>>>>>> 07faf6833b425c9e7edcab1eb02b59cf1c8727f1
 	});
-
 };
