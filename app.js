35c6d7c6ec4fa4ca20f31646e5f9c1a725942d28
Carlos Novack <carlosnovack@gmail.com>
2018-09-20 19:29:17 -0300

    Primeiro erro na nova interface
    Signed-off-by: Carlos Novack <carlosnovack@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 const dotenv = require('dotenv')
 const result = dotenv.config()
@@ -9,7 +10,9 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
   .include('app/routes')
